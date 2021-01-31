9141d30439a53d8ee9483f80cb1106510d442b2b
Romano Livi <romano_livi@outlook.com>
2021-01-31 23:37:46 +0000

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,21 +6,42 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
-
-    redirect to "pets/#{@pet.id}"
+    if !params["owner"]["name"].empty?
+      @owner = Owner.create(name: params["owner"]["name"])
+    else 
+      @owner = Owner.find(params["owner"]["id"])
+    end
+      @pet = Pet.create(name: params["pet"]["name"], owner: @owner)
+      redirect to "/pets/#{@pet.id}"
   end
 
   get '/pets/:id' do 
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
+  
+  get '/pets/:id/edit' do 
+    @pet = Pet.find(params[:id])
+    @owners = Owner.all
+    erb :'/pets/edit'
+  end
 
   patch '/pets/:id' do 
-
-    redirect to "pets/#{@pet.id}"
+    # binding.pry
+    @pet = Pet.find(params[:id])
+    @pet.name = params[:pet_name]
+    if params["owner"]["name"].empty?
+      @owner = Owner.find(params[:owner_id])
+    else 
+      @owner = Owner.create(name: params["owner"]["name"])
+    end
+    @pet.owner = @owner
+    @pet.save 
+    redirect to "/pets/#{@pet.id}"
   end
 end
