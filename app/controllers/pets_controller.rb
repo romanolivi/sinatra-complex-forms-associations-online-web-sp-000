b7d670bf587c184df903d66ce89ffb5cf2b02abe
Romano Livi <romano_livi@outlook.com>
2021-02-11 07:05:58 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -26,6 +26,7 @@ class PetsController < ApplicationController
   end
   
   get '/pets/:id/edit' do 
+    # binding.pry
     @pet = Pet.find(params[:id])
     @owners = Owner.all
     erb :'/pets/edit'
